a8546917419b410aa93c4b8c3ca55e8eddf57ae5
Saileshwar <saileshwar.bogam@zemosolabs.com>
2022-02-12 23:16:42 +0530

    intermediate

--- src/components/atoms/banner-image/BannerImage.test.tsx
+++ src/components/atoms/banner-image/BannerImage.test.tsx
@@ -1,7 +1,7 @@
 import { cleanup, render ,screen} from "@testing-library/react";
 import BannerImage from "./BannerImage";
-
 import React from 'react';
+import "@testing-library/jest-dom"
 afterEach(cleanup);
 it("Testing AvatarLogin",()=>{
     render(<BannerImage/>);
